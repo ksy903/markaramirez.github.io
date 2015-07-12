99ec91884f36349a981bd5d430898d2e4b314307
Mark Ramirez <markaramirez8@gmail.com>
2015-07-12 03:16:47 -0500

    added "buildings" to clicker game

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 var peanuts = 0;
+var elephants = 0;
 
 function performClick(num) {
   peanuts += num;
   document.getElementById("peanuts").innerHTML = peanuts;
 };
+
+function buyElephant() {
+  var elephantCost = Math.floor(10 * Math.pow(1.1, elephants));
+  if(peanuts >= elephantCost) {
+    elephants++;
+    peanuts -= elephantCost;
+    document.getElementById("elephants").innerHTML = elephants;
+    document.getElementById("elephantCost").innerHTML = elephantCost;
+  };
+  var nextCost = Math.floor(10 * Math.pow(1.1, elephants));
+  document.getElementById("elephantCost").innerHTML = nextCost;
+};
+
+window.setInterval(function(){
+  performClick(elephants);
+}, 1000);
