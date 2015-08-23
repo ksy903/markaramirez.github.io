3351bfae8137fb0593b72762f613ef45f11491dc
Mark Ramirez <markaramirez8@gmail.com>
2015-08-23 07:37:18 -0500

    fixed formulas bug

--- physics/mainscript.js
+++ physics/mainscript.js
@@ -19,7 +19,7 @@ var main = function() {
     }, 200);
   });
 
-  $(.'formula').click(function() {
+  $('.formula').click(function() {
     $('.jumbotron').css('background-image','url(http://www.yhpacademy.org/uploads/3/7/4/0/37401827/ap1_equation_sheet_page_2_53fd117ac240d.png)');
   });
 };
