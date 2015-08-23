1716bc8a24f1e5225c176ac191f422e2e92b2224
Mark Ramirez <markaramirez8@gmail.com>
2015-08-23 07:34:23 -0500

    updated menu links

--- physics/mainscript.js
+++ physics/mainscript.js
@@ -18,6 +18,10 @@ var main = function() {
       left: "0px"
     }, 200);
   });
+
+  $(.'formula').click(function() {
+    $('.jumbotron').css('background-image','url(http://www.yhpacademy.org/uploads/3/7/4/0/37401827/ap1_equation_sheet_page_2_53fd117ac240d.png)');
+  });
 };
 
 
