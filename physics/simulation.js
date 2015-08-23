9bea1630332de732785867f5c6a34b24967c5e8d
Mark Ramirez <markaramirez8@gmail.com>
2015-08-23 06:03:00 -0500

    used stable version of Matter

--- physics/simulation.js
+++ physics/simulation.js
@@ -3,6 +3,7 @@ var Engine = Matter.Engine,
   Bodies = Matter.bodies;
 
 var engine = Engine.create(document.body);
+
 var boxA = Bodies.rectangle(400, 200, 80, 80);
 var boxB = Bodies.rectangle(450, 50, 80, 80);
 var ground = Bodies.rectangle(400, 610, 810, 60 {isStatic: true});
