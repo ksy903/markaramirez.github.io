ed39cab4b1fdf28d21d004b101312f1414cbc1ee
Mark Ramirez <markaramirez8@gmail.com>
2015-08-23 06:06:01 -0500

    fixed base

--- physics/simulation.js
+++ physics/simulation.js
@@ -1,12 +1,12 @@
 var Engine = Matter.Engine,
-  World = Matter.world,
-  Bodies = Matter.bodies;
+    World = Matter.World,
+    Bodies = Matter.Bodies;
 
 var engine = Engine.create(document.body);
 
 var boxA = Bodies.rectangle(400, 200, 80, 80);
 var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60 {isStatic: true});
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 World.add(engine.world, [boxA, boxB, ground]);
 
