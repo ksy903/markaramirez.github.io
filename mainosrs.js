57c72cbf9a3468b08a74c17ddce04cda82be8139
Mark Ramirez <markaramirez8@gmail.com>
2015-09-12 19:46:24 -0500

    added osrs tools

---
+++ mainosrs.js
@@ -0,0 +1,53 @@
+var ts = 1;
+var nt = 0;
+
+function toggleSound() {
+  if(ts == 1) {
+    ts = 0;
+  }
+  else {
+    ts = 1;
+  }
+}
+
+function playGP() {
+  if(ts == 1) {
+    var sound = new Audio("gpsound.wav");
+    sound.play();
+    sound.currentTime = 0;
+  }
+}
+
+var main = function() {
+  $('.natthieflink').click(function() {
+      $('.nathief').toggle();
+      if(nt == 1) {
+        nt = 0;
+      }
+      else {
+        nt = 1;
+      }
+  });
+
+  $('.thievinglink').click(function() {
+      $('.thieving').toggle();
+  });
+};
+
+window.setInterval(function(){
+  if(nt == 1){
+    $('.nathiefpopup').toggle();
+    playGP();
+  }
+}, 8000);
+
+window.setInterval(function(){
+  $('.nathiefpopup').hide();
+}, 1000);
+
+$(document).ready( function(){
+  $('.nathiefpopup').hide();
+  $('.nathief').hide();
+  $('.thieving').hide();
+  main();
+});
