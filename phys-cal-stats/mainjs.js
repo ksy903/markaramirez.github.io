d8646dce3beb7e8ce950eb0872a369a84a0b3504
Mark Ramirez <markaramirez8@gmail.com>
2015-12-20 05:26:56 -0600

    ditched old physics app layout and used Snap.js and jqMath

---
+++ phys-cal-stats/mainjs.js
@@ -0,0 +1,16 @@
+var addEvent = function addEvent(element, eventName, func) {
+	if (element.addEventListener) {
+    	return element.addEventListener(eventName, func, false);
+    } else if (element.attachEvent) {
+        return element.attachEvent("on" + eventName, func);
+    }
+};
+
+addEvent(document.getElementById('open-left'), 'click', function(){
+  if( snapper.state().state=="left" ){
+        snapper.close();
+    } else {
+        snapper.open('left');
+    }
+
+});
